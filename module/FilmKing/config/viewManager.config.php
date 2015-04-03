e496cc71d70ccf14f20fefc0d4747cd7624be5b0
Chris Ramsden <1986chris@gmail.com>
2015-04-03 23:42:13 +0100

    Adding some basic nav

--- module/FilmKing/config/viewManager.config.php
+++ module/FilmKing/config/viewManager.config.php
@@ -6,12 +6,12 @@ return [
     'not_found_template'       => 'error/404',
     'exception_template'       => 'error/index',
     'template_map' => [
-        'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-        'home'          => __DIR__ . '/../view/filmking/index/index.phtml',
-        'films/index'   => __DIR__ . '/../view/filmking/film/index.phtml',
-        'films/film'    => __DIR__ . '/../view/filmking/film/film.phtml',
-        'error/404'     => __DIR__ . '/../view/error/404.phtml',
-        'error/index'   => __DIR__ . '/../view/error/index.phtml',
+        'layout/layout'     => __DIR__ . '/../view/layout/layout.phtml',
+        'home'              => __DIR__ . '/../view/filmking/index/index.phtml',
+        'films/index'       => __DIR__ . '/../view/filmking/film/index.phtml',
+        'films/film'        => __DIR__ . '/../view/filmking/film/film.phtml',
+        'error/404'         => __DIR__ . '/../view/error/404.phtml',
+        'error/index'       => __DIR__ . '/../view/error/index.phtml',
     ],
     'template_path_stack' => [
         __DIR__ . '/../view',
